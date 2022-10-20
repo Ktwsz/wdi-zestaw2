be99cb33e71c9b2ec15978c5074e6a7158e5ea6f
ktwsz <oktawiuszdoroszuk@gmail.com>
2022-10-20 13:12:20 +0200

    done 1

---
+++ 1.py
@@ -0,0 +1,22 @@
+from operator import truediv
+
+
+def gen(a, b, n):
+    c, d = a, b
+    while d <= 1000000:
+        c, d = d, c+d
+        if c * a == n:
+            return True
+    return False
+
+def foo(n):
+    a, b = 0, 1
+    while b <= 1000000:
+        a, b = b, a+b
+        if gen(a, b, n):
+            return True
+    return False
+
+print(foo(int(input())))
+
+
